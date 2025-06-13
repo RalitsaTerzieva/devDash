eb9ec1c619688887c215b105fc3021b2f9173021
Ralica Popova <ralicaterzieva90@gmail.com>
2025-06-13 18:11:14 +0300

    Remove comment

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,4 +1,4 @@
-// Regex and data structures
+
 export const searchWidgets = (widgets, query) => {
     const regex = new RegExp(query, 'i');
     return widgets.filter(widget => regex.test(widget.title));
