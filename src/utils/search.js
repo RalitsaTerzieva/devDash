2287f933ef0c76d61c33b0042b7f0cdd95cd7573
Ralica Popova <ralicaterzieva90@gmail.com>
2025-08-09 17:46:00 +0300

    Add comment to search utils

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,4 +1,14 @@
-
+/**
+ * Filters an array of widgets based on a search query.
+ * 
+ * - Creates a case-insensitive regular expression from the query.
+ * - Checks if each widget's `title` matches the query.
+ * - Returns only the widgets whose titles match.
+ * 
+ * @param {Array} widgets - Array of widget objects, each having a `title` property.
+ * @param {string} query - The search term to match against widget titles.
+ * @returns {Array} - A filtered array of matching widgets.
+ */
 export const searchWidgets = (widgets, query) => {
     const regex = new RegExp(query, 'i');
     return widgets.filter(widget => regex.test(widget.title));
